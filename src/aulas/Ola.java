18725f2683ba36235d062a65336bcda736dcf1d4
Ricardo <ricardovieirads@outlook.com>
2025-12-26 12:53:12 -0300

    first commit2

---
+++ src/aulas/Ola.java
@@ -0,0 +1,15 @@
+package aulas;
+
+public class Ola {
+
+    public static void main(String[] args){
+
+ String s = " aulas.Ola bom dia a Você! ";
+ s = s.replace("dia","noite");
+ s = s.toUpperCase();
+ s = s.concat("Senhor Ricardo");
+System.out.println(s);
+
+
+    }
+}
---
+++ src/aulas/importar.java
@@ -0,0 +1,28 @@
+package aulas;
+import java.util.Date;
+import java.util.Scanner;
+
+public class importar {
+
+    static void main(String[] args) {
+
+        Date d = new Date();
+        System.out.println(d);
+
+        //Strings
+        var s = "Ola mundo ";
+        System.out.println(s.toUpperCase().charAt(2));
+        System.out.println(s.length());
+        // Scanner
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite seu nome: ");
+        var test = input.nextLine();
+
+        System.out.println("Seu nome é: "+test);
+
+
+
+
+
+    }
+}
